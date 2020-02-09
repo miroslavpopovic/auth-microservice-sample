4ccb5016145e88df35846d0344b902f799ba8407
Miroslav Popovic <miroslav.popovic@gmail.com>
2020-02-09 18:27:03 +0100

    Add a missing Consent page to Auth project

---
+++ src/Auth/Extensions/IdentityServerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Stores;
+
+namespace Auth.Extensions
+{
+    public static class IdentityServerExtensions
+    {
+        /// <summary>
+        /// Determines whether the client is configured to use PKCE.
+        /// </summary>
+        /// <param name="store">The store.</param>
+        /// <param name="clientId">The client identifier.</param>
+        /// <returns></returns>
+        public static async Task<bool> IsPkceClientAsync(this IClientStore store, string clientId)
+        {
+            if (string.IsNullOrWhiteSpace(clientId)) return false;
+
+            var client = await store.FindEnabledClientByIdAsync(clientId);
+            return client?.RequirePkce == true;
+        }
+    }
+}
