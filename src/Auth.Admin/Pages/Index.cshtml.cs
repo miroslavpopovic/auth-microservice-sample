8bf930a13b2ecf0a4851b2579e72d88ed6381969
Miroslav Popovic <miroslav.popovic@gmail.com>
2020-02-09 01:11:25 +0100

    Add dashboard to admin

--- src/Auth.Admin/Pages/Index.cshtml.cs
+++ src/Auth.Admin/Pages/Index.cshtml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Auth.Admin.Pages
@@ -11,15 +10,25 @@ namespace Auth.Admin.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ConfigurationDbContext _dbContext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ConfigurationDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        public void OnGet()
-        {
+        public int ClientCount { get; set; }
+        public int ApiResourceCount { get; set; }
+        public int IdentityResourceCount { get; set; }
 
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ClientCount = await _dbContext.Clients.CountAsync();
+            ApiResourceCount = await _dbContext.ApiResources.CountAsync();
+            IdentityResourceCount = await _dbContext.IdentityResources.CountAsync();
+
+            return Page();
         }
     }
 }
