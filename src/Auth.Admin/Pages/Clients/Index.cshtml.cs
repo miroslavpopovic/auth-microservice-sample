772f6225ac21e3d32e19ddcb96ea6ddf40134d56
Miroslav Popovic <miroslav.popovic@gmail.com>
2020-02-19 15:29:42 +0100

    Add search and pagination to clients page

--- src/Auth.Admin/Pages/Clients/Index.cshtml.cs
+++ src/Auth.Admin/Pages/Clients/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Auth.Admin.Mappers;
@@ -17,16 +18,36 @@ namespace Auth.Admin.Pages.Clients
         [BindProperty]
         public IEnumerable<ClientModel> Clients { get; set; }
 
+        public int CurrentPage { get; set; }
+
+        public int PageSize { get; set; }
+
+        public string Search { get; set; }
+
+        public int TotalPages { get; set; }
+
         public IndexModel(ConfigurationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public async Task<IActionResult> OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(string search, int p = 1, int size = 10)
         {
-            var clients = await _dbContext.Clients.ToListAsync();
+            var clientCount = await _dbContext.Clients
+                .CountAsync(x => search == null || x.ClientId.Contains(search) || x.ClientName.Contains(search));
+
+            var clients = await _dbContext.Clients
+                .Where(x => search == null || x.ClientId.Contains(search) || x.ClientName.Contains(search))
+                .Skip((p - 1) * size)
+                .Take(size)
+                .ToListAsync();
 
             Clients = clients.Select(ClientMappers.ToModel);
+
+            CurrentPage = p;
+            PageSize = size;
+            Search = search;
+            TotalPages = (int) Math.Ceiling((decimal) clientCount / PageSize);
 
             return Page();
         }
